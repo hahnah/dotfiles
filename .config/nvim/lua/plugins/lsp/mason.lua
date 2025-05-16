001c0d4dd19bfe624894d946e9bab9179be0ae70
hahnah <superhahnah@gmail.com>
2025-05-16 20:27:00 +0900

    nvim: Update plugins and mason settings for new version.

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -1,44 +1,18 @@
 return {
   "williamboman/mason.nvim",
+  version = "*",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
-
-    mason.setup({
+    require("mason").setup({
       ui = {
         icons = {
           package_installed = "✓",
           package_pending = "➜",
           package_uninstalled = "✗",
         },
-      },
-    })
-
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "cssls",
-        "elmls",
-        "graphql",
-        "html",
-        "jsonls",
-        "lua_ls",
-        "markdown_oxide",
-        "ruby_lsp",
-        "volar",
-      },
-    })
-
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier",
-        "stylua",
-        "elm-format",
-        "eslint_d",
       },
     })
   end,
